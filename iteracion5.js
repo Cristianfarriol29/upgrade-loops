7f6fe493ef15363c642142239758aecde19a1ccb
cristian <cristianfarriol19@hotmail.com>
2022-03-03 09:51:18 +0100

    iteracion5

--- iteracion5.js
+++ iteracion5.js
@@ -10,8 +10,10 @@ const placesToTravel = [{id: 5, name: 'Japan'},
 for(let i=0; i < placesToTravel.length; i++ ){
 
 
-   if(placesToTravel[i].id !== 11 && placesToTravel[i].id !== 40){
-       console.log(placesToTravel[i])
+   if( placesToTravel[i].id === 11 || placesToTravel[i].id === 40 ){
+       placesToTravel.splice(i, 1)
    }
 
 }
+
+console.log(placesToTravel)
